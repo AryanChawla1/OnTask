9d5474bb1f9617b1738eecba3fc19a0d4b61ca91
Aryan Chawla <aryanchawlad@gmail.com>
2022-05-14 20:10:28 -0400

    Adding styling based on className

--- on-task/src/app/tasks/tasks.component.ts
+++ on-task/src/app/tasks/tasks.component.ts
@@ -7,12 +7,27 @@ import { Task } from '../task/task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks = [ new Task('item1', 'class1', new Date()), new Task('item2', 'class2', new Date()), new Task('item3', 'class 1', new Date())];
+  tasks = [ new Task('item1', 'class1', new Date()), new Task('item2', 'class2', new Date()), new Task('item3', 'class1', new Date())];
+  classes = ['class1', 'class2'];
 
   addTask(newTask: Task) {
+    if (!this.classes.includes(newTask.className)) {
+      this.classes.push(newTask.className);
+    }
     this.tasks.push(newTask);
   }
+
+  getStyling(task: Task) {
+    for (let i = 0; i < this.classes.length; i++) {
+      if (task.className === this.classes[i]) {
+        return i;
+      }
+    }
+    return;
+  }
+
   currentTask = 'Test';
+
   constructor() { }
 
   ngOnInit(): void {
