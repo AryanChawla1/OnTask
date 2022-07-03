9b603add3098d5e36a6d712d2f83a79a4e211c04
Aryan Chawla <aryanchawlad@gmail.com>
2022-07-02 21:21:23 -0400

    Routing Between tasks and edit works

--- on-task/src/app/app-routing.module.ts
+++ on-task/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditTaskComponent } from './edit-task/edit-task.component';
-
-const routes: Routes = [{ path: 'edit', component: EditTaskComponent }];
+import { TasksComponent } from './tasks/tasks.component';
+const routes: Routes = [
+   { path: 'edit', component: EditTaskComponent },
+   { path: 'tasks', component: TasksComponent },
+   { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+];
 
 @NgModule({
    imports: [RouterModule.forRoot(routes)],
